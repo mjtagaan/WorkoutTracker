f7ef53707d3319f11cb5d3f1c42cd3e6e91bc78c
MjTagaan <mj.tagaan@gmail.com>
2025-04-03 10:14:27 +0800

    Added list in to my gitignore.

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Enter commit message: " commit
-git add .
-git commit -m "$commit"
-git push origin master
-echo "Changes have been committed and pushed to the master branch."
